5525c330acde2527fccc488b80fb9c07f516d3f6
Eduardo Carneiro <duduccarneiro@gmail.com>
2024-10-10 15:27:38 -0300

    Avisando ao componente proposal que uma nova proposta foi criada

--- resources/views/components/proposals/item.blade.php
+++ resources/views/components/proposals/item.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div>
             <div class="text-white text-[14px] font-bold tracking-wide">
-                {{ $proposal->author }}
+                {{ $proposal->email }}
             </div>
             <div class="text-[#8C8C9A] text-[12px]">
                 Enviado {{ $proposal->created_at->diffForHumans() }}
@@ -18,6 +18,6 @@
     </div>
     <div class="uppercase font-bold text-[#C3C3D1] flex items-center space-x-2 px-[8px] py-[4px] rounded-full bg-[#181826] border border-[#1E1E2C] text-[12px]">
         <x-ui.icons.clock class="w-[18px] h-[18px]"/>
-        <span>{{ str_pad($proposal->time, 2, "0", STR_PAD_LEFT) }} horas</span>
+        <span>{{ str_pad($proposal->hours, 2, "0", STR_PAD_LEFT) }} horas</span>
     </div>
 </div>
